08ada73a2b3e9617c8178536e83a58950444c76c
spiderkiller13 <spiderkiller6@gmail.com>
2018-08-05 08:10:03 +0800

    init_commit

---
+++ test_udev.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+get_dev=$(ls /dev/ttyA*; ls /dev/ttyU*)
+
+declare -i idx=1 
+until [ -z "$(echo ${get_dev} | cut -d " " -f${idx})" ]
+do
+    dev_name=$(echo ${get_dev} | cut -d " " -f${idx})
+    idx=idx+1
+
+    dev_name=${dev_name:5}
+
+    devpath=$(udevadm info -a -n ${dev_name} | grep ATTRS{devpath}== | cut -d "\"" -f2)
+    devpath=$(echo ${devpath} | cut -d " " -f1)
+
+    idVendor=$(udevadm info -a -n  ${dev_name} | grep ATTRS{idVendor}== | cut -d "\"" -f2)
+    idVendor=$(echo ${idVendor} | cut -d " " -f1)
+
+    idProduct=$(udevadm info -a -n  ${dev_name} | grep ATTRS{idProduct}== | cut -d "\"" -f2)
+    idProduct=$(echo ${idProduct} | cut -d " " -f1)
+
+    product_name=$(lsusb | grep ${idVendor}:${idProduct})
+    product_name=${product_name:33}
+    echo " "
+    echo "************     "${product_name}"     ***************"
+    echo "dev_name  : "${dev_name}
+    echo "ATTRS{devpath}   : "${devpath}
+    echo "ATTRS{idVendor}  : "${idVendor}
+    echo "ATTRS{idProduct} : "${idProduct}
+done
