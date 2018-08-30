b052e390dd9475a1ddf1b21e1d0e3116bbd276bf
spiderkiller13 <spiderkiller6@gmail.com>
2018-08-30 14:06:21 +0800

    test on elevator_server, add symbolic info.

--- test_udev.sh
+++ test_udev.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-
 get_dev=$(ls /dev/ttyA*; ls /dev/ttyU*)
 
+echo "Finding USB device, this might take a while"
 declare -i idx=1 
 until [ -z "$(echo ${get_dev} | cut -d " " -f${idx})" ]
 do
@@ -20,10 +20,14 @@ do
 
     product_name=$(lsusb | grep ${idVendor}:${idProduct} | head -n 1)
     product_name=${product_name:33}
+
+    symlink=$(ls /dev/ -al | grep "\->" | grep ${dev_name} | awk '{print $9}')
     echo " "
-    echo "************     "${product_name}"     ***************"
-    echo "dev_name  : "${dev_name}
-    echo "ATTRS{devpath}   : "${devpath}
-    echo "ATTRS{idVendor}  : "${idVendor}
-    echo "ATTRS{idProduct} : "${idProduct}
+    echo -e "************  \033[1;33m${product_name}\033[0m       ***************"
+    echo -e "dev_name         : \033[1;31m${dev_name}\033[0m"
+    echo    "ATTRS{devpath}   : "${devpath}
+    echo    "ATTRS{idVendor}  : "${idVendor}
+    echo    "ATTRS{idProduct} : "${idProduct}
+    echo -e "SYMLINK          : \033[1;31m${symlink}\033[0m"
+
 done
