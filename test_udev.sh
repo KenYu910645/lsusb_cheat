aef7c0c300bd9b66834f593d3d23d35bcece6851
spiderkiller13 <spiderkiller6@gmail.com>
2018-08-31 09:53:58 +0800

    search product_name in both lsusb and udev

--- test_udev.sh
+++ test_udev.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-get_dev=$(ls /dev/ttyA*; ls /dev/ttyU*)
+get_dev=$(ls /dev/ttyA*; ls /dev/ttyU*; ls /dev/video*)
 
 echo "Finding USB device, this might take a while"
 declare -i idx=1 
@@ -7,20 +7,20 @@ until [ -z "$(echo ${get_dev} | cut -d " " -f${idx})" ]
 do
     dev_name=$(echo ${get_dev} | cut -d " " -f${idx})
     idx=idx+1
-
     dev_name=${dev_name:5}
-    devpath=$(udevadm info -a -n ${dev_name} | grep ATTRS{devpath}== | cut -d "\"" -f2)
-    devpath=$(echo ${devpath} | cut -d " " -f1)
     
-    idVendor=$(udevadm info -a -n  ${dev_name} | grep ATTRS{idVendor}== | cut -d "\"" -f2)
-    idVendor=$(echo ${idVendor} | cut -d " " -f1)
+    udevadm info -a -n ${dev_name} > udev_buff.txt
+    devpath=$(cat udev_buff.txt | grep ATTRS{devpath}== | cut -d "\"" -f2 | head -n 1)
+    
+    idVendor=$(cat udev_buff.txt | grep ATTRS{idVendor}== | cut -d "\"" -f2 | head -n 1)
 
-    idProduct=$(udevadm info -a -n  ${dev_name} | grep ATTRS{idProduct}== | cut -d "\"" -f2)
-    idProduct=$(echo ${idProduct} | cut -d " " -f1)
+    idProduct=$(cat udev_buff.txt | grep ATTRS{idProduct}== | cut -d "\"" -f2 | head -n 1)
 
     product_name=$(lsusb | grep ${idVendor}:${idProduct} | head -n 1)
     product_name=${product_name:33}
-
+    if [ ! -n "$product_name" ]; then
+        product_name=$(cat udev_buff.txt | grep ATTRS{product}== | cut -d "\"" -f2 | head -n 1)
+    fi
     symlink=$(ls /dev/ -al | grep "\->" | grep ${dev_name} | awk '{print $9}')
     echo " "
     echo -e "************  \033[1;33m${product_name}\033[0m       ***************"
